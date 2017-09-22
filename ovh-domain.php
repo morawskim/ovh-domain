e014f0c4da1c7089032de18fbe327f9bc160159d
Marcin Morawski <marcin@morawskim.pl>
2017-09-22 09:24:59 +0200

    main application

---
+++ ovh-domain.php
@@ -0,0 +1,11 @@
+#!/usr/bin/env php
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$application = new \Symfony\Component\Console\Application('ovh domain');
+$application->add(new \Mmo\OVHClient\Commands\ListDomainZonesCommand());
+$application->add(new \Mmo\OVHClient\Commands\ListDomainZoneRecords());
+$application->add(new \Mmo\OVHClient\Commands\UpdateDomainRecord());
+$application->add(new \Mmo\OVHClient\Commands\CurrentCredential());
+$application->run();
